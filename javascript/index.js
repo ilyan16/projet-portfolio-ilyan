b4b4375bbc5120b4216028f0e51514cc4ad88715
ilyan16 <i.clesse@yahoo.fr>
2023-12-15 09:18:08 +0100

    15/12/2023

--- javascript/index.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    let x = document.getElementById("Topnav");
-    console.log(x)
-    if (x.className === "topclass_bar") {
-      x.className += " responsive";
-    } else {
-      x.className = "Topnav";
-    }
-  }
